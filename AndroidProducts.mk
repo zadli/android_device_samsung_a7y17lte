9c3ce40931fc3ee6911d19c9ef41451487b92c14
Simon1511 <simon2002.schoenmackers@gmail.com>
2020-02-11 17:58:18 +0100

    Switch to common_lunch_choices

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,3 +15,8 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_a7y17lte.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_a7y17lte-userdebug \
+    lineage_a7y17lte-user \
+    lineage_a7y17lte-eng
